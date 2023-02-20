e17ca4289782a76bf887e46dbe692dee704637a2
Tamas Schell <tanksapka@yahoo.com>
2023-02-20 22:37:05 +0100

    Adding tracemalloc to measure memory usage. Optimizing polars csv import and group by method.

--- pandas_vs_polars/bm_group_by.py
+++ pandas_vs_polars/bm_group_by.py
@@ -12,6 +12,7 @@ import logging
 import pandas as pd
 import polars as pl
 import time
+import tracemalloc
 from pandas_vs_polars.utils import time_it
 from pathlib import Path
 
@@ -31,27 +32,33 @@ columns = (
 
 @time_it
 def pandas_group_by(file_path: Path):
+    tracemalloc.start()
     _logger.info(f"[pandas] running group by on file {file_path.name}")
     df = pd.read_csv(file_path, sep="\t")
+    mem_curr, mem_peak = tracemalloc.get_traced_memory()
+    _logger.info(f"[pandas] memory usage current: {mem_curr:,}, peak: {mem_peak:,}")
     _logger.info(f"[pandas] dataframe shape {df.shape}")
     for col in columns:
         _logger.info(f"[pandas] group by on {col}")
         tic = time.perf_counter_ns()
         result = df[['username', *col]].groupby(by=col, as_index=False).count()
         toc = time.perf_counter_ns() - tic
+        mem_curr, mem_peak = tracemalloc.get_traced_memory()
+        _logger.info(f"[pandas] memory usage current: {mem_curr:,}, peak: {mem_peak:,}")
         _logger.info(f"[pandas] group by took {toc:,} ns, shape {result.shape}")
-        # yield result
+    tracemalloc.stop()
 
 
 @time_it
 def polars_group_by(file_path: Path) -> None:
+    tracemalloc.start()
     _logger.info(f"[polars] running group by on file {file_path.name}")
-    df = pl.read_csv(file_path, sep="\t")
-    _logger.info(f"[polars] dataframe shape {df.shape}")
     for col in columns:
         _logger.info(f"[polars] group by on {col}")
         tic = time.perf_counter_ns()
-        result = df.lazy().select(['username', *col]).groupby(by=col).agg(pl.count()).collect()
+        result = pl.scan_csv(file_path, sep="\t").groupby(col).agg(pl.count().alias('username')).collect()
         toc = time.perf_counter_ns() - tic
+        mem_curr, mem_peak = tracemalloc.get_traced_memory()
+        _logger.info(f"[polars] memory usage current: {mem_curr:,}, peak: {mem_peak:,}")
         _logger.info(f"[polars] group by took {toc:,} ns, shape {result.shape}")
-        # yield result
+    tracemalloc.stop()
